573c5805789508fe7f6fc1aab4c281d2a9b1a63a
googlegis <googlegis@hotmail.com>
2023-02-01 15:05:52 +0800

    add readme

--- WebGL3D/02-01/js/GLUtil.js
+++ WebGL3D/02-01/js/GLUtil.js
@@ -1,49 +1,67 @@
+/**
+ * 初始化WebGL Canvas的方法
+ * @param canvasName
+ * @returns {*}
+ */
 function initWebGLCanvas(canvasName) {
-    canvas = document.getElementById(canvasName);
-    let context = canvas.getContext("webgl2",{antialias:true});
-    return context;
+    canvas = document.getElementById(canvasName); //获取Canvas对象
+    let context = canvas.getContext("webgl2",{antialias:true});//获取GL上下文
+    return context;//返回GL上下文对象
 }
 
+/**
+ * 加载单个着色的方法
+ * @param ctx
+ * @param shaderScript
+ * @returns {WebGLShader|null}
+ */
 function loadSingleShader(ctx, shaderScript) {
 
-    if(shaderScript.type == "vertex") {
-        var shaderType = ctx.VERTEX_SHADER;
-    } else if(shaderScript.type == "fragment") {
-        var shaderType = ctx.FRAGMENT_SHADER;
-    } else {
+    if(shaderScript.type == "vertex") { //若为顶点着色器
+        var shaderType = ctx.VERTEX_SHADER; //顶点着色器类型
+    } else if(shaderScript.type == "fragment") { //若为片元着色器
+        var shaderType = ctx.FRAGMENT_SHADER; //片元着色器类型
+    } else { //否则打印错误信息
         console.log("*** Error: shader script of undefined type '" + shaderScript.type + "'");
         return null;
     }
 
-    var shader = ctx.createShader(shaderType);
+    var shader = ctx.createShader(shaderType);//根据类型创建着色器程序， *** 变量 shaderType 需使用 var 类型，不要用let，否则此处代码不通。
 
-    ctx.shaderSource(shader, shaderScript.text);
+    ctx.shaderSource(shader, shaderScript.text); //加载着色器脚本
 
-    ctx.compileShader(shader);
+    ctx.compileShader(shader); //编译着色器
 
-    var compiled = ctx.getShaderParameter(shader, ctx.COMPILE_STATUS);
+    var compiled = ctx.getShaderParameter(shader, ctx.COMPILE_STATUS); //检查编译状态
 
-    if(!compiled && !ctx.isContextLost()) {
-        var error = ctx.getShaderInfoLog(shader);
+    if(!compiled && !ctx.isContextLost()) { //若编译出错
+        var error = ctx.getShaderInfoLog(shader); //获取错误信息
 
-        console.log(" *** Error compiling shader：" + error);
+        console.log(" *** Error compiling shader：" + error); //打印错误信息
 
-        ctx.deleteShader(shader);
-        return null;
+        ctx.deleteShader(shader); // 删除着色器程序
+        return null;//返回空
     }
-    return shader;
+    return shader; //返回着色器程序
 }
 
+/**
+ * 加载链接顶点、片元着色器的方法
+ * @param gl
+ * @param vshader
+ * @param fshader
+ * @returns {null|WebGLProgram}
+ */
 function loadShaderSerial(gl, vshader, fshader) {
-    var vertexShader = loadSingleShader(gl, vshader);
-    var fragmentShader = loadSingleShader(gl, fshader);
+    var vertexShader = loadSingleShader(gl, vshader); //加载顶点着色器
+    var fragmentShader = loadSingleShader(gl, fshader);//加载片元着色器
 
-    var program = gl.createProgram();
+    var program = gl.createProgram();//创建着色器程序
 
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
+    gl.attachShader(program, vertexShader);//将顶点着色器添加到着色器程序中
+    gl.attachShader(program, fragmentShader);//将片元着色器添加到着色器程序中
 
-    gl.linkProgram(program);
+    gl.linkProgram(program);//链接着色器程序
 
     var linked = gl.getProgramParameter(program,gl.LINK_STATUS);
     if(!linked && !gl.isContextLost()) {
--- WebGL3D/02-01/js/LoadShaderUtil.js
+++ WebGL3D/02-01/js/LoadShaderUtil.js
@@ -1,31 +1,41 @@
-function shaderObject(typeIn, textIn) {
-    this.type = typeIn;
-    this.text = textIn;
+function shaderObject(typeIn, textIn) { // 生命shaderObject类
+    this.type = typeIn; //初始化type成员变量
+    this.text = textIn; //初始化text成员变量
 }
 
-let shaderStrArray = ["a","a"];
+let shaderStrArray = ["a","a"]; //存储着色器数组
 
-let shaderNumberCount = 0;
+let shaderNumberCount = 0; //数组索引值
 
-let shaderTypeName = ["vertex","fragment"];
+let shaderTypeName = ["vertex","fragment"]; //着色器名称数组
 
+/**
+ * 处理着色器内容的回调函数
+ * @param req
+ * @param index
+ */
 function processLoadShader(req,index) {
-    if(req.readyState == 4) {
-        let shaderStr = req.responseText;
-        shaderStrArray[shaderNumberCount] = new shaderObject(shaderTypeName[shaderNumberCount], shaderStr);
-        shaderNumberCount++;
-        if(shaderNumberCount>1) {
+    if(req.readyState == 4) { //数据接收
+        let shaderStr = req.responseText; //获取响应文本
+        shaderStrArray[shaderNumberCount] = new shaderObject(shaderTypeName[shaderNumberCount], shaderStr); // 根据不同的数值索引值创建不同的着色器，并存入着色器数组
+        shaderNumberCount++; //数组索引值加1
+        if(shaderNumberCount>1) { //如果两个着色器内容均不为空，则加载着色器
             shaderProgArray[index] = loadShaderSerial(gl,shaderStrArray[0],shaderStrArray[1]);
         }
     }
 }
 
+/**
+ * 从服务器加载着色器脚本的函数
+ * @param url
+ * @param index
+ */
 function loadShaderFile(url, index) {
-    let req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        processLoadShader(req,index)
+    let req = new XMLHttpRequest(); // 创建XMLHttpRequest对象
+    req.onreadystatechange = function () { //设置响应回调函数
+        processLoadShader(req,index); // 调用processLoadShader处理响应
     }
-    req.open("GET",url,true);
-    req.responseType = "text";
-    req.send(null);
+    req.open("GET",url,true);//用GET方式打开指定URL
+    req.responseType = "text";//设置响应类型
+    req.send(null);//发送http请求
 }
