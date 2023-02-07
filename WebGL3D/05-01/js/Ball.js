1a799d52c03a3ed3107be037c3b047ebcf3236b2
googlegis <googlegis@hotmail.com>
2023-02-07 16:54:56 +0800

    add05

---
+++ WebGL3D/05-01/js/Ball.js
@@ -0,0 +1,14 @@
+/**
+ * 声明绘制用物体对象所属类
+ * @constructor
+ */
+function Ball(
+    gl,
+    programIn,
+    BallR
+) {
+    this.vertexData = new Array();
+    var r = BallR;
+    this.initVertex
+
+}
